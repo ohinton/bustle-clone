0491d8f7b7e14964d6ff27d3b5b645ea9c8a7109
Noah Yasskin and Olivia Hinton <noah@>
2016-08-16 13:56:47 -0700

    add news stories and photos

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+var news = [{
+  id: 1,
+  headline: "Newsy Headline",
+  type: "news",
+  image: "http://bit.ly/2bE3mZ4",
+  story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}, {
+  id: 2,
+  headline: "Other Newsy Headline",
+  type: "news",
+  image: "http://bit.ly/2aYfWT0",
+  story: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return news;
+  },
 });
